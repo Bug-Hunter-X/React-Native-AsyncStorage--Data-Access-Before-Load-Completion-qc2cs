f47aeb3777881f00ae748d6de3cfac7ea24fdbdb
BugHunter-X <askzenai@gmail.com>
2024-12-20 15:08:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when using AsyncStorage in React Native. The issue is that you are trying to access the AsyncStorage data before it has finished loading. This can lead to unexpected behavior, such as the app crashing or displaying incorrect data.
